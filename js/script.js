15fd1e1e5b5902aedfe030dbb4abd1e6d5bbef70
AsitSwain2002 <asitswain7327@gmil.com>
2024-01-02 20:19:35 +0530

    Bewakoof landing page

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let a = document.getElementById("abc")
+let b = document.querySelector(".search")
+a.addEventListener("click",()=>{
+    b.style.backgroundColor = "white"
+    b.style.border = "1px solid black"
+    // a.style.border = "none"
+})
